43d442d7d1f8065b273ce4c127024b9123ff9c4e
Vlad Matei <vlad.mateei@gmail.com>
2024-04-01 14:22:39 +0100

    trained linear regression model on male sub1h10 open results

--- hyrox_results_analysis.py
+++ hyrox_results_analysis.py
@@ -3,16 +3,20 @@ import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
 
 #  Global variables to be used for run and work/station labels throughout this analysis file!
 
 WORK_LABELS = []
 RUN_LABELS = []
+ROXZONE_LABELS = []
 STATIONS = ['SkiErg', 'SledPush', 'Sled Pull', 'Burpee Broad Jump', 'Rowing', 'Farmers Carry',
                       'Sandbag Lunges', 'Wall Balls']
 for i in range(1, 9):
     WORK_LABELS.append('work_' + str(i))
     RUN_LABELS.append('run_' + str(i))
+    ROXZONE_LABELS.append('roxzone_' + str(i))
 
 def load():
     path2csv = Path("hyroxData")
@@ -33,19 +37,24 @@ def get_filtered_df(df, column, value):
     subset_df = subset_df.sort_values(by='total_time', ascending=True)
     return subset_df
 
-
 def analyse_race_under_seventy(df):
     df = get_filtered_df(df, column='total_time', value='1:10:00')
 
-    df['total_time'] = pd.to_timedelta(df['total_time'])
+    df['Position'] = range(1, len(df) + 1)
 
+    for col in RUN_LABELS + WORK_LABELS + ROXZONE_LABELS + ["total_time"]:
+        df[col] = pd.to_timedelta(df[col])
+        df[col] = df[col].dt.total_seconds() / 60.0
 
     # let's plot all the values of each run
     extract_averages(df)
+    plot_distributions(df)
     plot_data_points(df, RUN_LABELS, 'Run analysis for Male Open, under 1:10', 'Runs')
     plot_data_points(df, WORK_LABELS, 'Station analysis for Male Open, under 1:10', 'Stations',
                      ['SkiErg', 'SledPush', 'Sled Pull', 'Burpee Broad Jump', 'Rowing', 'Farmers Carry',
                       'Sandbag Lunges', 'Wall Balls'])
+
+    linear_regression_model(df)
 
 def extract_averages(df):
     """
@@ -54,24 +63,24 @@ def extract_averages(df):
     :return:
     """
 
-    for col in RUN_LABELS + WORK_LABELS:
-        df[col] = pd.to_timedelta(df[col])
-        df[col] = df[col].dt.total_seconds()
+
+
 
     mean_values_run = [df[col].mean() for col in RUN_LABELS]
     mean_values_station = [df[col].mean() for col in WORK_LABELS]
-
+    mean_values_roxzone = [df[col].mean() for col in ROXZONE_LABELS]
     plt.figure(figsize=(10, 6))
 
     plt.plot(range(1, 9), mean_values_run, 'ro-', label="Mean value for runs")
     plt.plot(range(1, 9), mean_values_station, "gx-", label="Mean value for stations")
+    plt.plot(range(1, 9), mean_values_roxzone, 'y+-', label="Mean value for roxzones")
 
     plt.xlabel('Row/Station')
 
     xticks = [f'{run}/{station}' for run, station in zip(RUN_LABELS, STATIONS)]
 
-    plt.ylabel('Time (s)')
-    plt.title('Mean Values for Rows and Stations')
+    plt.ylabel('Time (minutes)')
+    plt.title('Avg Times for Runs and Stations')
 
     plt.xticks(range(1, 9), xticks, rotation=45)
     plt.grid(True)
@@ -107,7 +116,7 @@ def plot_data_points(df, columns_to_extract, title, xlabel, x_labels=None):
 
     # Set labels and title
     plt.xlabel(xlabel)
-    plt.ylabel('Time (s)')
+    plt.ylabel('Time (minutes)')
     plt.yticks(fontsize=6)
     plt.title(title)
 
@@ -121,12 +130,57 @@ def plot_data_points(df, columns_to_extract, title, xlabel, x_labels=None):
 
     return df
 
+def plot_distributions(df):
+    fig, axs = plt.subplots(2, 4, figsize=(12, 8))
+
+    for i, ax in enumerate(axs.flat):
+        ax.hist(df[f'run_{i+1}'], bins=10,color='blue', alpha=0.5, label=f'Run{i+1}')
+
+
+        ax.hist(df[f'work_{i+1}'], bins=10,color='red', alpha=0.5, label=STATIONS[i])
+
+        ax.set_xlabel('Time (minutes)')
+        ax.set_ylabel('Number of athletes')
+        ax.legend()
+
+    plt.title('Time distributions of each run and station')
+    plt.tight_layout()
+    plt.show()
+
+def linear_regression_model(df):
+
+    # CREATE THE X (predictors) and y (target)
+    X = df[RUN_LABELS + WORK_LABELS]
+    y = df['Position']
+
+    regr = LinearRegression()
+    regr.fit(X, y)
+
+    print('Intercept: \n', regr.intercept_)
+    print('Coefficients: \n', regr.coef_)
+
+    x = sm.add_constant(X) # adding a constant
+    model = sm.OLS(y, X).fit()
+    predictions = model.predict(X)
+
+    print_model = model.summary()
+    print(print_model)
+
+    plt.figure(figsize=(10, 6))
+    coefficients = pd.Series(model.params[1:], index=X.columns)
+    coefficients.plot(kind='bar')
+    plt.title('Coefficients of Predictors')
+    plt.xlabel('Predictors')
+    plt.ylabel('Coefficient')
+    plt.xticks(range(len(coefficients)), [entry for entry in (RUN_LABELS + STATIONS)])
+    plt.show()
+
 
 
 
 sns.set_style('darkgrid')
-barcelona2023 = load_one_file("hyroxData/S5 2023 London.csv")
-types = barcelona2023.dtypes
-barca_entry_male_open = get_division_entry(barcelona2023, 'male', 'open')
+london2023 = load_one_file("hyroxData/S5 2023 London.csv")
+types = london2023.dtypes
+barca_entry_male_open = get_division_entry(london2023, 'male', 'open')
 
 analyse_race_under_seventy(barca_entry_male_open)
